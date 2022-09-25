1da69316fac63ffe0d2b55216bee674fb621aed1
Saswat Swain <64731852+Saswat956@users.noreply.github.com>
2022-09-25 23:34:09 +0530

    Create Json.py

---
+++ Json.py
@@ -0,0 +1,22 @@
+id _mapping=( 'alation03|prod|schema 118451:
+'alation03|prod|schema 13279'
+Prey 68021 E9 new co 2 Car
+'alation03|prod|schema 9241'
+alation03|prod|schema 13199'1
+ク
+3
+print (id mapping)
+response=['filteredUserEntitlements': [('globalCatalogIdentifier':
+'alation03|prod|schema 11845', 'access': False, 'type
+'unknown '), ( 'globalCatalogIdentifier': 'alation03|prod|schema 9241°
+'access': False,
+'type':
+'unknown' }]]
+4
+5
+print (response)
+for i in list (response.values ()) [0]:
+for j,k in i.items ():
+for 1 in id mapping.keys () :
+if k==1:
+illist (i.values ()) .index (k) ]=id mapping [1]
